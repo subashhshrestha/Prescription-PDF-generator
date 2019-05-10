75f299d6e1cae50aac1a093e50e7536bb9e16b8d
Subash Shrestha <subashhshrestha10@gmail.com>
2019-05-10 16:12:20 +0545

    changed server file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/pdfgenerator'));
+app.use(express.static(__dirname + '/dist/pdfgenerator'));
 
 app.get('/*', function(req,res) {
     
